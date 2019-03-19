44cfa711674492479ddfa3946b216a96e2de9e74
yosradirgham <dirgham.yosra@gmail.com>
2019-03-19 11:30:40 +0100

    a python-based (flask) basic website

---
+++ website_flask/script.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask('__name__')
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/projects')
+def projects():
+    return render_template("projects.html")
+
+@app.route('/articles')
+def articles():
+    return render_template("articles.html")
+
+@app.route('/resume')
+def resume():
+    return render_template("resume.html")
+
+if __name__ == "__main__":
+    app.run(debug = True)
